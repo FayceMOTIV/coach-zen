c35f13bc3c0293a24dbcf3af0b2f7780b996d958
FayceMOTIV <smmafk01@gmail.com>
2026-01-04 20:36:10 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { LineChart, Line, XAxis, YAxis, ResponsiveContainer, Area, AreaChart } from 'recharts';
 
