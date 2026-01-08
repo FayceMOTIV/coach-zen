3d20e124fa09cc315db823ec3bf88fff95f8bc49
Faical kriouar <faicalkriouar@MacBook-Pro-de-Faical-2.local>
2026-01-08 19:56:58 +0100

    fix Safari auth with redirect mode

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"IJjm4sYRCXeHgkw2cVltnzq8Z3qpFmwm0gPPw97Yr0E=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RmCoboxeAZbHEXFJyPwxJIfxkDxQHTTvR2zBkEBz7tI=\"\n}"
